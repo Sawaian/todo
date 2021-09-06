18ac13b5e8ee7d9b82439e7a3b4a4bcabe69a6c4
Keith <Keith.macario@gmail.com>
2021-09-06 14:31:53 -0700

    Local storage and webpack work.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import { taskDomGen } from "./taskDom";
-// import { task } from "./taskForm";
+import { task } from "./taskForm";
 
 // const task = () => {
-    console.log("taskworks")
-    let submit = document.getElementById("submit");
+    // console.log("taskworks")
+    // let submit = document.getElementById("submit");
     
-    submit.addEventListener('click', ()=>{
-        let title = document.getElementById("titleDom").value;
-            localStorage.setItem('title', title);
-        console.log(localStorage.getItem(title));
-    })
+    // submit.addEventListener('click', ()=>{
+    //     let title = document.getElementById("titleDom").value;
+    //         localStorage.setItem('title', title);
+    //     console.log(localStorage.getItem(title));
+    // })
 
     // }
 
+    task.taskStorage();
+
 console.log("this page is working.");
 console.log("this update is working.");
-console.log("second update");
+console.log("fifth update");
--- src/taskForm.js
+++ src/taskForm.js
@@ -1,17 +1,23 @@
 
 
-const task = () => {
+const task = (() => {
+
+    function taskStorage(){
     console.log("taskworks")
     let submit = document.getElementById("submit");
-    
+
     submit.addEventListener('click', ()=>{
         let title = document.getElementById("titleDom").value;
             localStorage.setItem('title', title);
         console.log(localStorage.getItem(title));
-    })
+    })}
 
-    return title;
-
+    return{
+        taskStorage,
     }
+
+    })();
+
+
 
     export { task }
