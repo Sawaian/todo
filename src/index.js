6c4dcf37968a56252bba2587b75bdbe8abb23375
Keith <Keith.macario@gmail.com>
2021-09-06 14:26:13 -0700

    Local storage is working. Problem wit webpack bundle.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import { taskDomGen } from "./taskDom";
-import { task } from "./taskForm";
+// import { task } from "./taskForm";
+
+// const task = () => {
+    console.log("taskworks")
+    let submit = document.getElementById("submit");
+    
+    submit.addEventListener('click', ()=>{
+        let title = document.getElementById("titleDom").value;
+            localStorage.setItem('title', title);
+        console.log(localStorage.getItem(title));
+    })
+
+    // }
+
+console.log("this page is working.");
+console.log("this update is working.");
+console.log("second update");
--- src/taskForm.js
+++ src/taskForm.js
@@ -3,6 +3,7 @@
 const task = () => {
     console.log("taskworks")
     let submit = document.getElementById("submit");
+    
     submit.addEventListener('click', ()=>{
         let title = document.getElementById("titleDom").value;
             localStorage.setItem('title', title);
