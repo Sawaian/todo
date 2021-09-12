123f8aab80c9c9197a127c1482f2b624849e061b
Keith <Keith.macario@gmail.com>
2021-09-11 21:08:11 -0700

    Added the images to the sidebar.

--- src/sidebar.js
+++ src/sidebar.js
@@ -5,21 +5,42 @@ const sideBar = (() => {
     let today = document.createElement("div");
     let thisWeek = document.createElement("div");
     let projects = document.createElement("div");
+    let inboxImage = document.createElement("img");
+    let todayImage = document.createElement("img");
+    let calendarImage = document.createElement("img");
+
+
 
 
 
         sideBar.setAttribute("id", "sidebar");
         inbox.setAttribute("id", "inbox");
+        inboxImage.setAttribute("id", "inboxImage");
+
         today.setAttribute("id", "today");
+        todayImage.setAttribute("id","todayImage");
+        calendarImage.setAttribute("id","calendarImage");
         thisWeek.setAttribute("id", "thisWeek");
+        
         projects.setAttribute("id", "projects");
+        
+        inboxImage.src = "Inbox.png";
+        todayImage.src = "Today.png"
+        calendarImage.src ="calendar.png";
+        inbox.textContent = "Inbox";
+        today.textContent = "Today";
+        thisWeek.textContent ="This Week";
 
         content.appendChild(sideBar);
         sideBar.appendChild(inbox);
         sideBar.appendChild(today);
         sideBar.appendChild(thisWeek);
         sideBar.appendChild(projects);
+        
 
+        inbox.appendChild(inboxImage);
+        today.appendChild(todayImage);
+        thisWeek.appendChild(calendarImage);
     
 
         return {
