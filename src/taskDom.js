305c9709928e33725bdc4ccc1faf96cd238859bf
Keith <Keith.macario@gmail.com>
2021-10-08 16:01:37 -0700

    Fixing the submission task form

--- src/taskDom.js
+++ src/taskDom.js
@@ -58,6 +58,7 @@ const background = (() => {
 
 const taskForm = (() => {
     let form = document.createElement("div");
+        form.setAttribute("class", "submitTaskForm");
     
         // form.setAttribute("action", "submit.php");
 
